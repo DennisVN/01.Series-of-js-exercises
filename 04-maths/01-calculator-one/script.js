68815530340a6843e76e6c66177e0b6503729b6a
DennisVN <d.vannieuwenhuyse@gmail.com>
2021-06-15 14:16:07 +0200

    WIP add variables

--- 04-maths/01-calculator-one/script.js
+++ 04-maths/01-calculator-one/script.js
@@ -8,23 +8,30 @@
 
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
+var num1 = document.getElementById("op-one").value;
+var num2 = document.getElementById("op-two").value;
+var result = document.getElementById("").value;
 
 (function() {
     // to get the value of an input: document.getElementById("element-id").value
 
     document.getElementById("addition").addEventListener("click", function() {
         // perform an addition
+        var op = (document.getElementById("op-one" + "op-two").value);
     });
 
     document.getElementById("substraction").addEventListener("click", function() {
         // perform an substraction
+       
     });
 
     document.getElementById("multiplication").addEventListener("click", function() {
         // perform an multiplication
+        
     });
 
     document.getElementById("division").addEventListener("click", function() {
         // perform an division
+        /*var result = x / y;*/
     });
 })();
