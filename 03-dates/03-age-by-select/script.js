ef6612813d3fd8162f8958a98ffe65d0313a0656
DennisVN <d.vannieuwenhuyse@gmail.com>
2021-06-15 11:40:07 +0200

    WIP run button, make object with current bthdate

---
+++ 03-dates/03-age-by-select/script.js
@@ -0,0 +1,23 @@
+/* becode/javascript
+ *
+ * /04-dates/03-age-by-select/script.js - 4.3: calculateur d'âge
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    // your code here
+    //BUTTON RUN 
+    var yearElement = document.getElementById('dob-year');
+    var monthElement = document.getElementById('dob-month');
+    var dayElement = document.getElementById('dob-day');
+    // TODO : MAKE AN OBJECT WITH CURRENT BIRTHDATE
+    let birthday = new Date(yearElement.value, monthElement.value, dayElement.value);
+    console.log(birthday);
+
+})();
