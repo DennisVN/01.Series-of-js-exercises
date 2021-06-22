aef5d927d8e2b1429629c16b12c8f9fbee8ebceb
DennisVN <d.vannieuwenhuyse@gmail.com>
2021-06-22 15:42:01 +0200

    Done unbroke the second img src link and everything works.

--- 06-dom/05-hover-image/script.js
+++ 06-dom/05-hover-image/script.js
@@ -8,23 +8,17 @@
 
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
+var imgSource = document.querySelector("img").getAttribute("src");
+var newImgSource = document.querySelector("img").getAttribute("data-hover");
 
 (function() {
 
-    // your code here
-    // keep mouseover and mouseout separated !
-    // change source from img to data-hover source ;)
-    // no reset needed
-    var imgSource = document.querySelector("img").getAttribute("src");
-    var newImgSource = document.querySelector("img").getAttribute("data-hover");
-    //console.log(imgSource);
-    //console.log(newImgSource); // why null tho ? 
     document.querySelector("img").addEventListener("mouseover", function(event) {
-    console.log("scrolledy") //scroll over initiated; fix out
-    })
+        document.querySelector("img").setAttribute("src", newImgSource);
+    });
 
     document.querySelector("img").addEventListener("mouseout", function(event) {
-        console.log("bye") //scroll over initiated; fix out
-    })
+        document.querySelector("img").setAttribute("src", imgSource);
+    });
     
 })();
