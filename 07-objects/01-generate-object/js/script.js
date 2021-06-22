1d2a0b5c2cdbbe61c5fe685297d0f54f1ac5f671
DennisVN <d.vannieuwenhuyse@gmail.com>
2021-06-22 16:20:47 +0200

    Add object, declare properties, log array to test in console.

--- 07-objects/01-generate-object/js/script.js
+++ 07-objects/01-generate-object/js/script.js
@@ -8,7 +8,17 @@
 
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
+// DECLARE VARIABLES AND OBJECT
+var myObj = new Object();
+myObj.lastname = 'van Nieuwenhuyse';
+myObj.firstname = 'Dennis';
+myObj.age = '31';
+myObj.city = 'Antwerp';
+myObj.county = 'Belgium';
+
+console.log([myObj]);
 
 (() => {
     // your code here
+    // THEN LOG HERE IN ARRAY 
 })();
