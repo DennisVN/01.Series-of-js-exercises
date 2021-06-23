7ead764df84d8d62c9896bc087796b28ccbecc6b
DennisVN <d.vannieuwenhuyse@gmail.com>
2021-06-23 14:42:41 +0200

    Done Fetch API and log array in console) git push <

--- 08-fetch/01-list-to-console/js/script.js
+++ 08-fetch/01-list-to-console/js/script.js
@@ -7,6 +7,9 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(() => {
-    // your code here
-})();
+
+(async () => {
+    let data = await fetch('http://127.0.0.1:5500/_shared/api.json');
+    let main = await data.json();
+    console.log(main);
+})();  
