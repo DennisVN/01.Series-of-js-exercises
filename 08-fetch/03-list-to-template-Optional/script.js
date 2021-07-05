29fac3d7d3948584639930ace578af062eaf4801
DennisVN <d.vannieuwenhuyse@gmail.com>
2021-07-05 15:33:41 +0200

    Finish later

--- 08-fetch/03-list-to-template-Optional/script.js
+++ 08-fetch/03-list-to-template-Optional/script.js
@@ -6,9 +6,32 @@
 
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
+const heroDiv = document.getElementById("tpl-hero");
+const heroHero = document.querySelector("hero");
+const heroName = document.querySelector("name");
+const alterEgo = document.querySelector("alter-ego");
+const heroPowers = document.querySelector("powers");
+const target = document.getElementById("target");
+
+
 document.getElementById("run").addEventListener("click", function() {
+    let target = document.getElementById("target");
     fetch ("http://127.0.0.1:5500/_shared/api.json")
     .then (response => response.json())
-    .then (data => console.log(data));
-
+    .then (heroes => {displayResults(heroes)})
 })
+
+function displayResults (heroes) {
+    heroHero.innerHTML = `${heroes.heroes[0].name}`;
+}
+
+
+
+/*showHeroes = heroes => {
+    const heroDiv = getElementById("#tpl-hero")
+    heroes.forEach( heroes => {
+        const heroElement = document.querySelector("hero");
+        heroElement.innerHTML = `${hero.name}`;
+        heroDiv.append(heroElement);
+    });
+}*/
