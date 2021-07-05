96283d220ca229959b8336bdd2eab71df88b6896
DennisVN <d.vannieuwenhuyse@gmail.com>
2021-07-05 11:20:27 +0200

    Link button to fetch API and console.log data

--- 08-fetch/03-list-to-template-Optional/script.js
+++ 08-fetch/03-list-to-template-Optional/script.js
@@ -7,5 +7,8 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 document.getElementById("run").addEventListener("click", function() {
-    console.log("Clickedy");
+    fetch ("http://127.0.0.1:5500/_shared/api.json")
+    .then (response => response.json())
+    .then (data => console.log(data));
+
 })
