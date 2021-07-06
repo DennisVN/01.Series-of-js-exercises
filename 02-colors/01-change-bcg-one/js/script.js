d780efd72ddac84beb8c1273378c05dfa265cbcc
DennisVN <d.vannieuwenhuyse@gmail.com>
2021-07-06 13:43:39 +0200

    Correct JS link in HTML, function red works onclick, WIP complete other colors same way

--- 02-colors/01-change-bcg-one/js/script.js
+++ 02-colors/01-change-bcg-one/js/script.js
@@ -10,4 +10,7 @@
 // You will have time to focus on it later.
 
 (function() {
+    document.getElementById("red").onclick = function() {
+        document.body.style.backgroundColor = "red";
+    }
 })();
